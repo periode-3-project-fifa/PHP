82e0395f3ad939d1138e926a8835964d3fcfdb70
stijn versluis <stijn.versluis@gmail.com>
2019-04-15 11:03:56 +0200

    navigation index

--- PHP/index.php
+++ PHP/index.php
@@ -10,6 +10,41 @@ $pagetitle = 'Home';
 $pagename = 'index';
 require 'header.php';
 ?>
+<div class="container">
+    <div class="navigation">
+        <h1>VoetbalPoules</h1>
+        <div class="login_register">
+            <a href="login.php">Login</a>
+            <a href="register.php">Register</a>
+        </div>
+    </div>
+</div>
+    <div class="main_top">
+        <div class="uitslagen">
+            <h3>Uitslagen</h3>
+            <div class="uitslag">
 
+            </div>
+        </div>
+        <div class="teams">
+            <h3>Teams</h3>
+        </div>
+        <div class="spelers">
+            <h3>Spelers</h3>
+        </div>
+    </div>
+    <div class="main_bottom">
+        <div class="poules">
 
+        </div>
+        <div class="quaterfinals">
+
+        </div>
+        <div class="halffinals">
+
+        </div>
+        <div class="finals">
+
+        </div>
+    </div>
 <?php require 'footer.php'; ?>
