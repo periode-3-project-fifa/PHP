5bfa25daa495ee7b42ca2f0199a90ac60d7f56e8
stijn versluis <stijn.versluis@gmail.com>
2019-04-16 12:35:43 +0200

    kaas

--- PHP/header.php
+++ PHP/header.php
@@ -19,4 +19,3 @@ require '../includes/config.php';
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/main.css">
     <link rel="stylesheet" href="../css/normalize.css">
-
