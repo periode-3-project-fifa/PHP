986a5cb56103817f3fe47c02e36f26dc42d86bf5
Unknown <batinsimsek1998@gmail.com>
2019-04-16 09:02:21 +0200

    Register pagina
    Register pagina is klaar helemaal. Als er veranderingen moeten komen even doorgeven

--- PHP/register.php
+++ PHP/register.php
@@ -27,24 +27,24 @@ require 'config.php'
     
 <main class = "register">
     <div class="container">
-        <h2>Register FIFA</h2>
-        <form action="loginController.php" method="post">
-            <input type="hidden" name="type" value="register">
+        <div class="background-register">
+            <h2>Register FIFA</h2>
+            <form action="loginController.php" method="post">
+                <input type="hidden" name="type" value="register">
+                    <div class="container">
+                        <label for="email"><b class="register-email">Email</b></label>
+                        <input type="email" placeholder="Enter Email" name="email" id="email" >
 
-            <div class="container">
-                <label for="email"><b>Email</b></label>
-                <input type="email" placeholder="Enter Email" name="email" id="email" >
+                        <label for="psw"><b class="register-password">Password</b></label>
+                        <input type="password" placeholder="Enter Password" name="password">
 
-                <label for="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="password">
+                        <label for="psw"><b class="register-password-reapeat">Voer opnieuw Password in</b></label>
+                        <input type="password" placeholder="Enter Password again" name="passwordconfirm" >
 
-                <label for="psw"><b>Voer opnieuw Password in</b></label>
-                <input type="password" placeholder="Enter Password again" name="passwordconfirm" >
-
-                <button class= "registerbutton" type="submit" value="Create new contact">Register</button>
-            </div>
-
-        </form>
+                        <button class= "registerbutton" type="submit" value="Create new contact">Register</button>
+                    </div>
+            </form>
+        </div>
     </div>
 </main>
 
