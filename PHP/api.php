93ff70a7038638088c0859866e482274fa30e06b
Unknown <robinadriaensen9@gmail.com>
2019-04-18 10:29:35 +0200

    Api Weer aangemaakt
    Hopelijk saved hij nu wel

--- PHP/api.php
+++ PHP/api.php
@@ -5,4 +5,12 @@
  * Date: 18-4-2019
  * Time: 09:51
  */
+require 'config.php';
+$sql = "SELECT * FROM teams";
+$query =$db->query($sql);
+$teams = $query->fetchAll(PDO::FETCH_ASSOC);
+
 ('Content-Type: application/json');
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+
+echo json_encode($arr);
