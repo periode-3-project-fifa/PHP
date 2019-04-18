5deb0a525863daccb5b1584d087faddd1e22327e
Unknown <batinsimsek1998@gmail.com>
2019-04-18 09:06:18 +0200

    teamplayer

---
+++ teamplayer.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: stijn versluis
+ * Date: 4/15/2019
+ * Time: 10:15 AM
+ */
+
+$pagename = 'teanplayer';
+$pagetitle = 'teamplayer';
+require 'header.php';?>
+    </head>
+<body class="<?=$pagename?>">
+
+<?php require 'footer.php'; ?>
+
+<header class = "register-header">
+        <div class="container">
+            <div class="register-navigation">
+                <h1>FIFA Voetbal Toernooi Register</h1>
+                <div class="register-controler">
+                    <a href="index.php" class="border-login">Home</a>   
+                </div>
+            </div>
+        </div>
+</header>
