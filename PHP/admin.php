aadf35997c4336bd6806894c3e785a72de465821
Unknown <robinadriaensen9@gmail.com>
2019-05-13 10:55:14 +0200

    Match planning

--- PHP/admin.php
+++ PHP/admin.php
@@ -18,16 +18,52 @@ if($_SESSION['admin'] != 1){
 ?>
 </head>
 <body>
-<ul>
+<ol>
+    <?php
+    foreach ($teams as $team){
+        $teamname = $team['name'];
+        $teamid = $team['id'];
+        echo "<li><a href='admindetail.php?id=$teamid'>$teamname</a></li>";
+    }
+    ?>
+</ol>
+
 <?php
-foreach ($teams as $team){
-    $teamname = $team['name'];
-    $teamid = $team['id'];
-    echo "<li><a href='admindetail.php?id=$teamid'>$teamname met ID is $teamid</a></li>";
-}
+    foreach ($teams as $team) {
+        $teamNameList[] = $team['name'];
+        $members = $teamNameList;
+        $schedule = scheduler($members);
+    }
+
+    function scheduler($members){
+        if (count($members) != 10){
+            array_push($members,"not enough teams");
+        }
+        $away = array_splice($members,(count($members)/2));
+        $home = $members;
+        for ($i=0; $i < count($home)+count($away)-1; $i++){
+            for ($j=0; $j<count($home); $j++){
+                $round[$i][$j]["Home"]=$home[$j];
+                $round[$i][$j]["Away"]=$away[$j];
+            }
+            if(count($home)+count($away)-1 > 2){
+                array_unshift($away,array_shift(array_splice($home,1,1)));
+                array_push($home,array_pop($away));
+            }
+        }
+        return $round;
+
+    }
+
+
+    foreach($schedule AS $round => $games){
+    echo "Round: ".($round+1)."<BR>";
+    foreach($games AS $play){
+        echo $play["Home"]." - ".$play["Away"]."<BR>";
+    }
+    echo "<BR>";
+    }
+
 ?>
-    <form action="loginController.php" method="post">
-        <input type="button" value="shuffle">
-    </form>
-</ul>
+
 <?= require 'footer.php';?>
