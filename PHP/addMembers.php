535c7a5e6d21f2793d4cc2e0c12c9c9e8f241d8f
stijn versluis <stijn.versluis@gmail.com>
2019-06-03 10:30:52 +0200

    max width adding players

--- PHP/addMembers.php
+++ PHP/addMembers.php
@@ -30,7 +30,7 @@ $team = $prepare->fetch(PDO::FETCH_ASSOC);
 echo "<div class='container-3'>";
 
 for ($i = 1; $i <= $playercount; $i++){
-    echo "<div class='container-4'>";
+    echo "<div class='container-4' style='max-width: 100px'>";
         echo "<label for='Player'><b>Speler $i</b></label>";
         echo "<input required type='text' name='player$i' placeholder='Voer de naam van speler $i in'>";
         echo "</div>";
